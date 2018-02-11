dff7b4c3d3b479db694f57952cd5cdf49a0b75c2
Joao Gabriel Gouveia <joao.gabriel.gouveia2@gmail.com>
2018-02-11 16:30:57 -0200

    Transformations

--- OpenGL/Shader.cpp
+++ OpenGL/Shader.cpp
@@ -95,3 +95,7 @@ void Shader::setInt(const std::string &name, int value) const {
 void Shader::setFloat(const std::string &name, float value) const {
 	glUniform1f(glGetUniformLocation(Id, name.c_str()), value);
 }
+
+void Shader::setMatrix4f(const std::string &name, const GLfloat *value) const {
+	glUniformMatrix4fv(glGetUniformLocation(Id, name.c_str()), 1, GL_FALSE, value);
+}
